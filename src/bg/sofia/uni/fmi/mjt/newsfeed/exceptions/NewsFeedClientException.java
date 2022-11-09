f4a5e929dd694324e06b569962247725af396f32
nikoleschi <73163972+nikoleschi@users.noreply.github.com>
2022-11-09 18:31:38 +0200

    Create NewsFeedClientException.java

---
+++ src/bg/sofia/uni/fmi/mjt/newsfeed/exceptions/NewsFeedClientException.java
@@ -0,0 +1,7 @@
+package bg.sofia.uni.fmi.mjt.newsfeed.exceptions;
+ 
+public class NewsFeedClientException extends Exception {
+    public NewsFeedClientException(String message) {
+        super(message);
+    }
+}
