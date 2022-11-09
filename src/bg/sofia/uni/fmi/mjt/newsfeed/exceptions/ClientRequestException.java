b642fe096a5105d9396eb352ec1e536f13366964
nikoleschi <73163972+nikoleschi@users.noreply.github.com>
2022-11-09 18:30:43 +0200

    Create ClientRequestException.java

---
+++ src/bg/sofia/uni/fmi/mjt/newsfeed/exceptions/ClientRequestException.java
@@ -0,0 +1,9 @@
+package bg.sofia.uni.fmi.mjt.newsfeed.exceptions;
+ 
+public class ClientRequestException extends NewsFeedClientException {
+ 
+    public ClientRequestException(String message) {
+        super(message);
+    }
+}
+ 
